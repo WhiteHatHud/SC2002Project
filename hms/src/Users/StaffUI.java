1eba872218f3ecd42b62e789c11e9ef5bae89ace
Dlhwai <106801652+Dlhwai@users.noreply.github.com>
2024-10-31 23:11:02 +0800

    Created staff, staffData and staffUI classes. added Staff_list.csv

--- hms/src/Users/StaffUI.java
+++ hms/src/Users/StaffUI.java
@@ -8,21 +8,36 @@ public class StaffUI extends StaffData{
 
     public void displayAllStaff(){
         List<Staff> staffList = dataInterface.getAllStaff();
-        for (Staff temp : staffList){
-            print(temp);
+        for (Staff staff : staffList){
+            print(staff);
         }
     }
-    public void print(Staff temp){
+    public void print(Staff staff){
         System.out.println(
-            temp.userID + ", " +
-            temp.name + ", " +
-            temp.role + ", " +
-            temp.gender + ", " +
-            temp.age
+            staff.userID + ", " +
+            staff.name + ", " +
+            staff.role + ", " +
+            staff.gender + ", " +
+            staff.age
         );
+    }
+    public void deleteStaff(String staffID){
+        if (dataInterface.removeStaffByID(staffID)){
+            System.out.println(staffID + " deleted from Staff List");
+            return;
+        }
+        System.out.println("Unable to find staff with matching ID");
+    }
+    public void checkStaff(String staffID){
+        Staff staff;
+        if((staff = dataInterface.getStaffByID(staffID)) != null){
+            print(staff);
+            return;
+        }
+        System.out.println("Unable to find staff with matching ID");
     }
     public static void main(String[] args) {
         StaffUI ui = new StaffUI();
-        ui.displayAllStaff();
+        ui.checkStaff("D002");
     }
 }
