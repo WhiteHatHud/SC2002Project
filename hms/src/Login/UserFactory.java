7bf2b5a52a52df566869d382b7180e34ce69101f
Whitehathud <whitehathud@gmail.com>
2024-11-15 10:39:35 +0800

    Update UserFactory.java

--- hms/src/Login/UserFactory.java
+++ hms/src/Login/UserFactory.java
@@ -1,9 +1,24 @@
 package Login;
+
 import Patients.Patient;
 import Users.Users;
 
+/**
+ * Factory class for creating instances of {@link Users} based on the specified user type.
+ * This class uses a factory design pattern to centralize the creation of different user types.
+ */
 public class UserFactory {
 
+    /**
+     * Creates a {@link Users} object based on the specified user type and data.
+     * Currently supports the "patient" user type. Additional user types can be added as needed.
+     *
+     * @param userType The type of user to create (e.g., "patient").
+     * @param data     An array of Strings containing the user data fields. The specific fields
+     *                 required depend on the user type.
+     * @return A {@link Users} instance of the specified type, populated with the provided data.
+     * @throws IllegalArgumentException if the user type is unknown or unsupported.
+     */
     public static Users createUser(String userType, String[] data) {
         switch (userType.toLowerCase()) {
             case "patient":
